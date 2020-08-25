0e25e4647e03db5c4305386f2d885cb35859ddea
MilicaSaveski <70214570+MilicaSaveski@users.noreply.github.com>
2020-08-25 18:04:32 +0200

    Update aboutme_content.php

--- view/aboutme_content.php
+++ view/aboutme_content.php
@@ -8,10 +8,9 @@
                         <div class="about-title">
                             <span>10 Mart 2020</span>
                             <h2>Ja sam Milica Saveski, <br />53/16</h2>
-                            <p>U produzenoj sam godini na Visoka ICT i nadam se da cu ove godine zavrsiti, bar su mi 
-							 to bili planovi pre ovog vanrednog stanja. Imala sam muke sa php i bezala sam od toga. A kad vise nisam imala gde da bezim, onda sam sela i krenula da ucim sve ispocetka.
-							 Po mojoj gradji se ne moze videti da volim hranu, ali stvarno volim hranu, da kuvam i spremam za moju porodicu.</p>
-                            <p></p>
+                            
+                            <p>student završne godine Visoke ICT škole. 
+				</p>
                         </div>
                     </div>
                 </div>
